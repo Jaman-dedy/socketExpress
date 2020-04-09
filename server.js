6935234aa615437de0a8a731b0ac78f5b33b6c70
Jaman Dedy <jeandedieuam@gmail.com>
2020-04-09 17:58:58 +0200

    first part

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,11 +17,58 @@ const io = socketIo(server);
 // set static folder
 
 app.use(express.static(path.join(__dirname, 'public')));
+const botName = '2UMoney Bot';
 
 // RUn when a client connects
 
 io.on('connection', (socket) => {
-  console.log('new WS connection');
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
+
+    // welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to 2uMoney'));
+
+    // Broadcast when a user connects
+
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  //Listen for chatMessage
+
+  socket.on('chatMessage', (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // RUns when client disconnects
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
